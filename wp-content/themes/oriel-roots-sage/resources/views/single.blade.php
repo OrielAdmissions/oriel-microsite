0555e436378f873df5a485615d893f34b30ad96a
Sean Khottavong <sean.khottavong@gmail.com>
2025-03-28 12:21:05 -0400

    Update single post template

--- wp-content/themes/oriel-roots-sage/resources/views/single.blade.php
+++ wp-content/themes/oriel-roots-sage/resources/views/single.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  @while (have_posts())
-    @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+    @while (have_posts())
+        @php(the_post())
+        @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
+    @endwhile
 
+    @if(is_singular('post'))
+        @include('partials.modules.latest-posts')
+    @endif
 @endsection
